c29c48fb1ee32b270ba94069dd9bfda6c0319b16
Matheus Alves <matheusalv.oliveira@gmail.com>
2024-04-03 15:40:26 -0300

    function controller first ver.

--- src/controllers/functionController.js
+++ src/controllers/functionController.js
@@ -1,10 +1,24 @@
 import food from "../models/food.js";
-import foodController from "./foodController.js";
+
 
 class functionController {
 
     static async sumByKg (req, res) {
-        
+        let sum = 0;
+
+        try {
+            const listFoods = await food.find({});
+            res.status(200).json(listFoods);
+
+            
+            for(const item of listFoods) {
+                sum+= parseFloat(item.kg.replace(/\./g, '').replace(/,/, '.'));
+            }
+
+        } catch (error) {
+            res.status(500).json({ message: `${error.message} - falha na requisição de rações.` })
+        }
+        return sum;
     }
 }
 
--- src/routes/foodRoutes.js
+++ src/routes/foodRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import foodController from "../controllers/foodController.js"
+import functionController from "../controllers/functionController.js";
 
 const ROUTES = express.Router();
 
@@ -9,4 +10,5 @@ ROUTES.put("/food/:id", foodController.editFood);
 ROUTES.delete("/food/:id", foodController.deleteFood);
 ROUTES.get("/food/:id", foodController.getFoodById); 
 
+ROUTES.get("/calculate", functionController.sumByKg)
 export default ROUTES;
