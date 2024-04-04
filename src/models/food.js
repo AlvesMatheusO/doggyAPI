5e43095fb107d38ee56e84def1d408b8daa5cd54
Matheus Alves <matheusalv.oliveira@gmail.com>
2024-04-04 11:22:28 -0300

    model changed to add date

--- src/models/food.js
+++ src/models/food.js
@@ -5,6 +5,7 @@ const FOOD_SCHEMA = new mongoose.Schema({
 
     id: {type: mongoose.Schema.Types.ObjectId},
     brand: {type: String, required: true},
+    date: {type: Date, default: Date.now}, // in case I want to manipulate date data I have to format it
     kg: {type: Decimal128, required: true},
     price: {type: Decimal128, required: true},
     price_kg: {type: Decimal128, required: true}
